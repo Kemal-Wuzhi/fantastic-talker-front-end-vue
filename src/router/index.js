2c0f7344c836795c1116bc5139b15fafbb627221
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-06-06 19:36:51 +0800

    modify: delete home & about pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router"
 // 每次載入時都會看到
-import HomeView from "../views/HomeView.vue"
+
 import NotFound from "../views/NotFound.vue"
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-    },
-    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
-  ],
+  routes: [{ path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound }],
 })
 
 export default router
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>404 Page Not Found</h1>
+  <div class="container">
+    <div class="mx-auto my-auto">
+      <h1>404 Page Not Found.</h1>
+    </div>
+  </div>
 </template>
