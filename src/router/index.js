c9be514b48effb3478014290263b0d821699d44b
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-06-06 22:20:26 +0800

    feat:add signin page feature

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router"
 // 每次載入時都會看到
-
+import SignIn from "../views/SignIn.vue"
 import NotFound from "../views/NotFound.vue"
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound }],
+  routes: [
+    {
+      path: "/",
+      name: "root",
+      redirect: "/signin",
+    },
+    {
+      path: "/signin",
+      name: "signin",
+      component: SignIn,
+    },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+  ],
 })
 
 export default router
